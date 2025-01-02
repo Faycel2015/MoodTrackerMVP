9969ddba84c0a8a8620124a954f8fc2e8a2b01ee
Faycel2015 <faycel.ayech@gmail.com>
2025-01-02 18:54:35 +0100

    initial xcode changes

---
+++ Mood Tracker/Mood Tracker/Mood_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mood_TrackerApp.swift
+//  Mood Tracker
+//
+//  Created by FayTek on 1/2/25.
+//
+
+import SwiftUI
+
+@main
+struct Mood_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
